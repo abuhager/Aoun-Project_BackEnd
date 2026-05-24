6bb9fbd5efe3618bb432a70c049cedfdcb318cdb
Adham <abuhager360@gmail.com>
2026-05-24 20:12:47 +0300

    fix(items): Phase 1 - compound indexes + migration script FIXES: #14

---
+++ scripts/buildItemIndexes.js
@@ -0,0 +1,11 @@
+﻿require('dotenv').config();
+const mongoose = require('mongoose');
+const Item     = require('../models/Item');
+
+(async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log('جارٍ بناء فهارس Item...');
+  await Item.syncIndexes();
+  console.log('تم بناء الفهارس بنجاح');
+  process.exit(0);
+})();
